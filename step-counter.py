0409f87cc6403ac5ec86bd398bf24c0d90ab164f
caden-miller <cadenmiller40@gmail.com>
2024-02-23 12:42:29 -0600

    second iteration - fleshed w no graphing

--- step-counter.py
+++ step-counter.py
@@ -18,12 +18,48 @@ data_acceleration = []
 frequency = 1000
 period = 1 / frequency
 
-while True:
-    # Read acceleration
-    acceleration = mpu.acceleration
-    data_acceleration.append(acceleration)
-    time.sleep(period)
-    if len(data_acceleration) == 100:
-        break
+def read_acceleration(num_points):
+    while True:
+        # Read acceleration
+        acceleration = mpu.acceleration
+        data_acceleration.append(acceleration)
+        time.sleep(period)
+        if len(data_acceleration) == num_points:
+            break
+    return data_acceleration
 
+# Create list to store the derivative of the acceleration
 derivative_acceleration = []
+
+def calculate_derivative(data_acceleration):
+    derivative_acceleration = np.diff(data_acceleration) / period
+    return derivative_acceleration
+
+# Find minimums and maximums
+local_minimums = []
+local_maximums = []
+
+def find_minimums_maximums(derivative_acceleration):
+    for i in range(1, len(derivative_acceleration) - 1):
+        if derivative_acceleration[i] < derivative_acceleration[i - 1] and derivative_acceleration[i] < derivative_acceleration[i + 1]:
+            local_minimums.append(derivative_acceleration[i])
+        elif derivative_acceleration[i] > derivative_acceleration[i - 1] and derivative_acceleration[i] > derivative_acceleration[i + 1]:
+            local_maximums.append(derivative_acceleration[i])
+    return local_minimums, local_maximums
+
+# Main loop
+num_steps = 0
+while True:
+    num_points = 100
+    read_acceleration(num_points)
+    derivative_acceleration = calculate_derivative(data_acceleration)
+    local_minimums, local_maximums = find_minimums_maximums(derivative_acceleration)
+    # print local minimums and maximums
+    print("Local minimums: ", local_minimums)
+    print("Local maximums: ", local_maximums)
+    if len(local_minimums) > 0:
+        num_steps += 1
+        print("Number of steps: ", num_steps)
+    if len(local_maximums) > 0:
+        num_steps += 1
+        print("Number of steps: ", num_steps)
